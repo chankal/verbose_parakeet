fb5ce1a494bb725b99145dd9d516f58b0cd3fa8a
chandini <76880500+chankal@users.noreply.github.com>
2021-08-17 12:54:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,23 @@ else:
     soup = BeautifulSoup(ans.text, 'html.parser')
     about_article = soup.find('div', role = 'note').text
 print(about_article)
+
+def top_report():
+    url = 'https://en.wikipedia.org/wiki/Wikipedia:Top_25_Report'
+    ans = requests.get(url)
+    soup = BeautifulSoup(ans.text, 'html.parser')
+    top_table = soup.find('dl').table.tbody
+    rows = top_table.find_all('tr')
+    for i, row in enumerate(rows):
+        data = row.find_all('td')
+        for index, info in enumerate(data):
+            if index == 1:
+                print(i, ': ', info.text)
+                break
+
+user_purpose = input('What do you want to find on Wikipedia? top report/myarticle\n' )
+if user_purpose =='my article':
+    user_article()
+
+else: 
+    top_report()
